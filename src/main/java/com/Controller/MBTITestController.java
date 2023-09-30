6564b6c2d8df4b57b9fe5fa00755d1fa3be33efc
hwoss123 <306031875@qq.com>
2023-09-30 21:35:00 +0800

    状态码完善
    状态码完善

--- src/main/java/com/Controller/MBTITestController.java
+++ src/main/java/com/Controller/MBTITestController.java
@@ -3,6 +3,7 @@ package com.Controller;
 import com.Service.MBTITestService;
 import com.pojo.Question;
 import com.pojo.Result;
+import com.utils.Code;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,10 +26,14 @@ public class MBTITestController {
     @GetMapping("/{num}")
     public Result getMBTIQuestions(@PathVariable Integer num){
         log.info("题目的个数:{}",num);
+        try{
+            List<Question> list = mbtiTestService.getQuestionsByQNum(num);
+            return Result.success(Code.MBTI_QUESTION_OK,list);
+        }catch(Exception e){
+            e.printStackTrace();
+            return Result.error(Code.MBTI_QUESTION_ERR,"获取题目失败");
+        }
 
-        List<Question> list = mbtiTestService.getQuestionsByQNum(num);
-
-        return Result.success(list);
     }
 
 }
